3d0f108192462d6887b027406b9caf5d525bb111
madhvesh23 <madhuchokshi23@gmail.com>
2023-12-30 14:50:38 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+ 
+  return (
+   <>
+   <View>
+    <Text>
+      hii
+    </Text>
+   </View>
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
